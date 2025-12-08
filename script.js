9603bdf59f150622ddc8b0c8677910c471a125b7
Yashwanth M <byg2281999@gmail.com>
2025-12-08 23:32:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+// Try different extensions if MP3 doesn't work
+function playSound(soundName) {
+    const extensions = ['.mp3', '.wav', '.ogg'];
+    let audioFound = false;
+    
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+    
+    // Try different file extensions
+    for (let ext of extensions) {
+        const audio = new Audio(`./sounds/${soundName}${ext}`);
+        audio.onerror = function() {
+            // Try next extension
+        };
+        audio.oncanplaythrough = function() {
+            if (!audioFound) {
+                audioFound = true;
+                currentAudio = audio;
+                currentAudio.play();
+            }
+        };
+        audio.load();
+    }
+}
